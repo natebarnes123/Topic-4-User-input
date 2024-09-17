ed9366262df3752d9b0f714a249db71a2a427b06
Nathan Barnes <339723215@GoTVDSB.ca>
2024-09-17 09:39:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Topic_4._User_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //New user input
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
