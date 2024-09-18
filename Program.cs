62866541d718c513ab3bfd070d30e46842de5fe6
Nathan Barnes <339723215@GoTVDSB.ca>
2024-09-18 09:34:09 -0400

    Didnt get started on topic 4 yet

--- Program.cs
+++ Program.cs
@@ -5,11 +5,24 @@
         static void Main(string[] args)
         {
             //New user input
-
-
-
-
-
+            string name;
+            Console.WriteLine("Hello, what is your name?:");
+            Console.WriteLine();
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            double price;
+            Console.WriteLine("What is the price?:");
+            double.TryParse(Console.ReadLine(), out price);
+            Console.WriteLine("The price is " + (price / 5).ToString("C"));
+            Console.ReadLine();
 
 
 
